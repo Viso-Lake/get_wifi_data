a3396c732593c5c2a49f1193c83b5a54306c34d1
Shrike <felixkot00@gmail.com>
2022-10-07 00:29:28 +0500

    New repo wifi_pass

---
+++ get_wifi_passwords.py
@@ -0,0 +1,27 @@
+import subprocess
+
+def extract_wifi_passwords():
+    profiles_data = subprocess.check_output('netsh wlan show profiles').decode('cp866').split('\n')
+
+    profiles = [i.split(':')[1].strip() for i in profiles_data if 'All User Profile' in i]
+    
+    with open('wifi_passwords.txt', 'w') as f:
+        for profile in profiles:
+                f.writelines(f'Логин: {profile}\n')
+                profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('cp866').split('\n')
+                try:
+                    password = [i.split(':')[1].strip() for i in profile_info if 'Key Content' in i][0]
+                    f.write(f'Пароль: {password}\n\n')
+                except IndexError:
+                    password = None
+                    f.write('Пароль: None \n\n')
+                print(f'Profile: {profile}\nPassword: {password}\n\n')
+    print("ALL DATA SAVED IN FILE wifi_passwords.txt")
+        
+
+
+
+
+
+
+extract_wifi_passwords()
